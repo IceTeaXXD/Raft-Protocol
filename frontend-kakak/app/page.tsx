e8dfae700b7ff9c4fdced8dea7f86a185288450c
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-06-01 17:48:36 +0700

    feat(ui): base ui terminal

--- frontend-kakak/app/page.tsx
+++ frontend-kakak/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import { useState, useRef, useEffect } from 'react';
+
+const PromptInput = ({ prompt, handleInput, index, processInput, inputRefs }: { prompt: string, handleInput: (event: React.ChangeEvent<HTMLInputElement>, index: number) => void, index: number, processInput: (index: number) => void, inputRefs: React.RefObject<HTMLInputElement[]> }) => {
+  const [readOnly, setReadOnly] = useState(false);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      processInput(index);
+      setReadOnly(true);
+
+      const nextIndex = index + 1;
+      if (inputRefs.current && inputRefs.current[nextIndex]) {
+        inputRefs.current[nextIndex].focus();
+      }
+    }
+  };
+
+  return (
+    <div className="flex gap-2 items-center justify-start font-ProFontIIxNerdFontRegular text-white my-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="ml-1 w-6 h-6"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="#c6aae8"
+          stroke="#22202c"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+          d="M9 10h.01M15 10h.01M12 2a8 8 0 0 0-8 8v12l3-3l2.5 2.5L12 19l2.5 2.5L17 19l3 3V10a8 8 0 0 0-8-8"
+        />
+      </svg>
+      <h1 className="text-black text-sm font-bold">$</h1>
+      <input
+        ref={el => { 
+          if (inputRefs.current) {
+            inputRefs.current[index] = el!;
+          }
+        }}
+        className="border-0 outline-0 text-sm text-black w-full mr-2"
+        type="text"
+        value={prompt}
+        readOnly={readOnly}
+        onChange={(event) => handleInput(event, index)}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
+};
+
+const Home = () => {
+  const [prompts, setPrompts] = useState([{ input: '', output: ''}]);
+  const inputRefs = useRef<HTMLInputElement[]>([]);
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const newPrompts = [...prompts];
+    newPrompts[index].input = event.target.value;
+    setPrompts(newPrompts);
+  };
+
+  const processInput = (index: number) => {
+    const processedInput = prompts[index].input;
+    const newPrompts = [...prompts];
+    newPrompts[index].output = processedInput;
+    setPrompts([...newPrompts, { input: '', output: '' }]);
+  };
+
+  useEffect(() => {
+    if (inputRefs.current.length > 0) {
+      inputRefs.current[inputRefs.current.length - 1].focus();
+    }
+  }, [prompts]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="w-[96%] h-[90%] md:w-[700px] md:h-[500px] lg:w-[800px] lg:h-[600px] flex flex-col border-8 border-black rounded-lg bg-black">
+          <div className="relative flex flex-col items-center">
+          </div>
+          <div className="w-full flex-1 bg-white pt-2 overflow-y-scroll scrollbar">
+            <h1 className="hidden md:block pl-2 my-2 font-profontiixnerdfont text-black text-sm">
+              Test
+            </h1>
+            <div>
+              {prompts.map((prompt, index) => (
+                <div key={index}>
+                  <PromptInput prompt={prompt.input} handleInput={handleInput} index={index} processInput={processInput} inputRefs={inputRefs} />
+                  {prompt.output && (
+                    <div className="pl-2 text-black font-profontiixnerdfont w-full break-words">
+                      Processed command: {prompt.output}
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
-}
+};
+
+export default Home;
